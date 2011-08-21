6849304c29332f531b420e2c581f6689180c8656
Joss Crowcroft <josscrowcroft@gmail.com>
2011-08-21 14:23:00 +0800

    some cleanup and fixes

--- accounting.js
+++ accounting.js
@@ -5,7 +5,32 @@
  * Copyright 2011 by Joss Crowcroft
  * Licensed under GPL v3 | http://www.gnu.org/licenses/gpl-3.0.txt
  */
-var accounting = (function() {
+var accounting = (function () {
+	
+	/**
+	 * Removes currency formatting from a number or array of numbers, returning numeric values
+	 * 
+	 * Decimal must be included in the regular expression to match floats (default: ".")
+	 */
+	function unformat(number, decimal) {
+		// Recursively unformat arrays:
+		if (typeof number === "object" && number.length > 1) {
+			for (var i = 0, values = []; i < number.length;
+				values.push(unformat(number[i], decimal)), i++
+			)
+			return values;
+		}
+		
+		// Default decimal point is "." but could be set to eg. ",":
+	    decimal = decimal || ".";
+	    
+	    // Build regex to strip out everything except digits, decimal point and minus sign:
+		var regex = new RegExp("[^0-9-" + decimal + "]", ["g"]);
+		
+		// Perform the unformatting regex if number is a single value:
+		return parseFloat(("" + number).replace(regex, '').replace(decimal, '.'));
+	}
+	
 	
 	/**
 	 * Format a number, with comma-separated thousands and custom precision/decimal places
@@ -14,10 +39,10 @@ var accounting = (function() {
 	 */
 	function formatNumber(number, precision, thousand, decimal) {
 		// Resursively format arrays:
-		if ( typeof number === "object" && number.length > 1 ) {
+		if (typeof number === "object" && number.length > 1) {
 			for (var i = 0, values = []; i < number.length;
-				values.push( formatNumber(number[i], precision, thousand, decimal) ) && i++
-			);
+				values.push(formatNumber(number[i], precision, thousand, decimal)) && i++
+			)
 			return values;
 		}
 		
@@ -29,7 +54,7 @@ var accounting = (function() {
 		
 		// Do some calc:
 		var negative = number < 0 ? "-" : "",
-		    base = parseInt(number = Math.abs(+number || 0).toFixed(precision)) + "",
+		    base = parseInt(number = Math.abs(+number || 0).toFixed(precision), 10) + "",
 		    mod = base.length > 3 ? base.length % 3 : 0;
 		
 		// Format the number:
@@ -47,8 +72,10 @@ var accounting = (function() {
 	 * Second param can be an object with keys matching the param names
 	 */
 	function formatMoney(number, symbol, precision, thousand, decimal) {
+		var symbolAfter;
+		
 		// Second param can be an object:
-		if ( typeof symbol === "object" ) {
+		if (typeof symbol === "object") {
 			precision = symbol.precision;
 			thousand = symbol.thousand;
 			decimal = symbol.decimal;
@@ -63,8 +90,9 @@ var accounting = (function() {
 		symbol = symbol !== undefined ? symbol : "$";
 		
 		// Default  symbol position is before value:
-		if ( typeof symbolAfter === "undefined" )
-			var symbolAfter = false;
+		if (typeof symbolAfter === "undefined") {
+			symbolAfter = false;
+		}
 		
 		// Format the number:
 		var formatted = formatNumber(number, precision, thousand, decimal);
@@ -87,64 +115,46 @@ var accounting = (function() {
 	 * browsers from collapsing the whitespace in the output strings.
 	 */
 	function formatColumn(list, symbol, precision, thousand, decimal) {
-		if ( !list )
+		if (!list) {
 			return [];
+		}
 		
 		var maxLength = 0,
-		    formatted = [];
+		    formatted = [],
+		    i;
 		
 		// Format the numbers according to options, and store the length of
 		// the longest string in the array:
-		for ( var i = 0; i < list.length; i++ ) {
-			formatted.push( formatMoney(list[i], symbol, precision, thousand, decimal) );
+		for (i = 0; i < list.length; i++) {
+			formatted.push(formatMoney(list[i], symbol, precision, thousand, decimal));
 			
-			if ( formatted[i].length > maxLength )
+			if (formatted[i].length > maxLength) {
 				maxLength = formatted[i].length;
+			}
 		}
 		
 		
 		// Second param can be an object, but symbol is needed for next part:
-		if ( typeof symbol === "object" ) symbol = symbol.symbol
-		if ( typeof symbol === undefined ) symbol = "$";
+		if (typeof symbol === "object") {
+			symbol = symbol.symbol;
+		}
+		if (typeof symbol === undefined) {
+			symbol = "$";
+		}
 		
 		// Add space between currency symbol and number to pad strings:
-		for ( var i = 0; i < formatted.length; i++ ) {
-			if ( formatted[i].length < maxLength ) {
+		for (i = 0; i < formatted.length; i++) {
+			if (formatted[i].length < maxLength) {
 				// Match first number in string and add enough padding:
 				formatted[i] = formatted[i].replace(
 					/(-?\d+)/,
-					(new Array( ( maxLength-formatted[i].length ) + 1 ).join(" ") ) + "$1"
+					(new Array((maxLength - formatted[i].length) + 1).join(" ")) + "$1"
 				);
 			}
 		}
 		
 		// Send back the list of numbers:			
 		return formatted;
-	}
-	
-	
-	/**
-	 * Removes currency formatting from a number or array of numbers, returning numeric values
-	 * 
-	 * Decimal must be included in the regular expression to match floats (default: ".")
-	 */
-	function unformat(number, decimal) {
-		// Recursively unformat arrays:
-		if ( typeof number === "object" && number.length > 1 ) {
-			for ( var i = 0, values = []; i < number.length;
-				values.push( unformat(number[i], decimal) ), i++
-			);
-			return values;
-		}
-		
-		// Default decimal point is "." but could be set to eg. ",":
-	    decimal = decimal || ".";
-	    
-	    // Build regex to strip out everything except digits, decimal point and minus sign:
-		var regex = new RegExp("[^0-9-" + decimal + "]", ["g"]);
-		
-		// Perform the unformatting regex if number is a single value:
-		return parseFloat(("" + number).replace(regex, '').replace(decimal, '.'));
 	}
 	
 	
@@ -155,4 +165,4 @@ var accounting = (function() {
 		formatColumn: formatColumn,
 		unformat: unformat
 	};
-}())
+}());
