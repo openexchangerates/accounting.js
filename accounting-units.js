0c7ade32c291b495137608c8b013aba8ee23f2ca
Joss Crowcroft <josscrowcroft@gmail.com>
2011-10-05 16:20:30 +0800

    cleanup

--- accounting-units.js
+++ accounting-units.js
@@ -1,6 +1,6 @@
 /**
  * accounting-units.js [alpha]
- * 
+ *
  * Adds a formatUnits method to the accounting.js library (load after accounting.js)
  */
 
@@ -8,14 +8,14 @@
  * Format large values into units (eg. 100K, 12GB)
  *
  * `base` is the division between units (e.g. 1000 for thousands, 1024 for bytes, etc.)
- * `units` is a string or array of strings to apply to each level of division 
+ * `units` is a string or array of strings to apply to each level of division
  * (e.g. ["K", "M", "B"] for base 1000)
  * `callback` is a function to apply to the value before returning, with the number and the
  * relevant unit as params. Can be used to format the number/currency before adding the unit.
- * 
+ *
  * Examples (see docs for more info):
  *     accounting.formatUnits(12345678, 1000, ["K", "M", "B"], function(num, units) {
- *         return accounting.formatNumber(num, 3) + units; 
+ *         return accounting.formatNumber(num, 3) + units;
  *     }); // "12.346M"
  *     accounting.formatUnits(123456, 1000000, "M"); // only return millions
  *     accounting.formatUnits(12345678, 1024, ["KB", "MB", "GB"]); // bytes
@@ -27,7 +27,7 @@ accounting.formatUnits = function(number, base, units, callback) {
 	units = typeof units === "string" ? [units] : units || [""];
 
 	// These are used later:
-	var unit, 
+	var unit,
 		neg = number < 0;
 
 	// If the number is smaller than base, divide it and add the first or only unit:
