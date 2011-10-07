2dcdba165827f3b5fea0148318e372e057e45bfa
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-10-06 21:01:28 -0400

    Should now be able to set values with $.accounting('settings', settingsOject);

--- accounting-jquery.js
+++ accounting-jquery.js
@@ -1,8 +1,18 @@
 ( function( $ ){
 	var methods = {
-		/*init: function( options ) {
-			//TODO: Some way to initialize the plug-in with defaults.
-		},*/
+		settings: function ( options ) {
+			//Fetch library's settings object.
+			var settings = accounting.settings;
+
+			//Merge in our new options
+			settings = $.extend(settings, options);
+
+			//Update library's settings object.
+			accounting.settings = settings;
+			
+			//Keep the jQuery chain going.
+			return this;
+		},
 		formatMoney: function( options ) {
 			return this.each(function() {
 				$this = $(this);
@@ -84,8 +94,8 @@
 		// Method calling logic
 		if ( methods[method] ) {
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		/*} else if ( typeof method === 'object' || ! method ) {
-			return methods.init.apply( this, arguments );*/
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.settings.apply( this, arguments );
 		} else {
 			$.error( 'Method ' +  method + ' does not exist on jQuery.accounting' );
 		}    
