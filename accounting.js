c619c5f1979b110e2a52fedc25603c6f5f76f92f
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-10-18 13:28:33 -0400

    No longer forcing precision to be positive.

--- accounting.js
+++ accounting.js
@@ -113,7 +113,7 @@
 	 * Check and normalise the value of precision (must be positive integer)
 	 */
 	function checkPrecision(val, base) {
-		val = Math.round(Math.abs(val));
+		val = Math.round(val);
 		return isNaN(val)? base : val;
 	}
 	
