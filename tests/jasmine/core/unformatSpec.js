291a235e533ef27ec8172177067dd6519ee0f588
Lakshmi-Sharma <lakshmisharma92@gmail.com>
2015-11-20 22:27:43 +0530

    Test cases for negative number and brackets, with combinations

--- tests/jasmine/core/unformatSpec.js
+++ tests/jasmine/core/unformatSpec.js
@@ -13,7 +13,24 @@ describe('unformat()', function(){
         expect( accounting.unformat('&*()$ -123,456') ).toBe( -123456 );
         expect( accounting.unformat(';$@#$%^&-123,456.78') ).toBe( -123456.78 );
     });
-    
+
+    it('should handle negative numbers and brackets', function(){
+        expect( accounting.unformat('(123,456)') ).toBe( -123456 );
+        expect( accounting.unformat('(123)456') ).toBe( -123456 );
+        expect( accounting.unformat('(IND)123') ).toBe( 123 );
+        expect( accounting.unformat('IND(123)') ).toBe( -123 );
+        expect( accounting.unformat('-(123)') ).toBe( 123 );
+        expect( accounting.unformat('(-123)') ).toBe( 123 );
+        expect( accounting.unformat('(1,234.56)') ).toBe( -1234.56 );
+    });
+
+    it('should handle negative deciaml numbers within brackets', function(){
+        expect( accounting.settings.number.deciaml = '@'; 
+                accounting.unformat('(1,234@56)') ).toBe( -1234.56 );
+        expect( accounting.settings.number.deciaml = '@'; 
+                accounting.unformat('(1,234.56)') ).toBe( -123456 );
+    });
+
     it('should accept different decimal separators', function(){    
         expect( accounting.unformat('$ 123,456', ',') ).toBe( 123.456 );
         expect( accounting.unformat('$ 123456|78', '|') ).toBe( 123456.78 );
